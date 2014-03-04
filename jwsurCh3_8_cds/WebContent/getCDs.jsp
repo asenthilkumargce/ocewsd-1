627412e674532797f44d4ad96cd7359934488cbe
etravassos <myemail@hotmail.com>
2014-03-04 14:56:01 -0300

    updating

---
+++ jwsurCh3_8_cds/WebContent/getCDs.jsp
@@ -0,0 +1,10 @@
+<jsp:useBean id    = "fetcher" 
+	     type  = "com.ocewsd.FetchXML" 
+	     class = "com.ocewsd.FetchXML">  
+</jsp:useBean> 
+<jsp:getProperty name = "fetcher" property = "json" />
+
+
+
+
+
---
+++ jwsurCh3_8_cds/src/com/ocewsd/FetchXML.java
@@ -0,0 +1,47 @@
+package com.ocewsd;
+
+//import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class FetchXML {
+	// public void setJson(JSONObject json) { } // no-op
+	public void setJson(String json) {
+	}
+
+	// public JSONObject getJson() {
+	public String getJson() {
+		System.out.println("Fetching JSON/XML..");
+		JSONObject json = null;
+		try {
+			// Fetch the XML document from the W3C site.
+			String xml = "";
+			URL url = new URL("http://www.w3schools.com/xml/cd_catalog.xml");
+			URLConnection conn = url.openConnection();
+			InputStream is = conn.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader in = new BufferedReader(isr);
+
+			// Read the document records.
+			String line = null;
+			while ((line = in.readLine()) != null)
+				xml += line;
+			in.close();
+
+			// Clean up the XML.
+			xml = xml.replace("'", "");
+
+			// Transform the XML document into a JSON object,
+			// in this case an array of song objects.
+			json = XML.toJSONObject(xml.toLowerCase());
+		} catch (Exception e) {
+		}
+		// return json;
+		return json.toString(); // JSON document
+	}
+}
